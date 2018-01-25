39630f04c486a69bd7229f2c9a6232155eb0d36b
Erickson Silva <erickson.lucas@live.com>
2018-01-25 21:11:04 -0200

    13. Pickup Objective Actor

--- StealthGame/Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ StealthGame/Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -3,13 +3,12 @@
 #include "FPSObjectiveActor.h"
 #include "Components/StaticMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "FPSCharacter.h"
 #include "Components/SphereComponent.h"
 
 AFPSObjectiveActor::AFPSObjectiveActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+ 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	RootComponent = MeshComp;
 
@@ -30,14 +29,15 @@ void AFPSObjectiveActor::PlayEffects()
 	UGameplayStatics::SpawnEmitterAtLocation(this, PickUpFX, GetActorLocation());
 }
 
-void AFPSObjectiveActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
-
 void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor * OtherActor)
 {
 	Super::NotifyActorBeginOverlap(OtherActor);
 	PlayEffects();
+	AFPSCharacter* MyCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (MyCharacter) 
+	{
+		MyCharacter->bIsCarryingObjective = true;
+		Destroy();
+	}
 }
 
