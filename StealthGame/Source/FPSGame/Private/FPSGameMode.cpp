501fc707e1f04df269d574315f2834895678be90
Erickson Silva <erickson.lucas@live.com>
2018-01-28 14:17:24 -0200

    40. Network the Game State Part 3

--- StealthGame/Source/FPSGame/Private/FPSGameMode.cpp
+++ StealthGame/Source/FPSGame/Private/FPSGameMode.cpp
@@ -29,13 +29,15 @@ void AFPSGameMode::CompleteMission(APawn* InstigatorPawn, bool bMissionSuccess)
 		
 		if (ReturnedActors.Num() > 0)
 		{
-			AActor* NewViewTarget = nullptr;
+			AActor* NewViewTarget = ReturnedActors[0];
 
-			NewViewTarget = ReturnedActors[0];
-			APlayerController* PC = Cast<APlayerController>(InstigatorPawn->GetController());
-			if (PC)
+			for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; It++)
 			{
-				PC->SetViewTargetWithBlend(NewViewTarget, 0.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+				APlayerController* PC = It->Get();
+				if (PC)
+				{
+					PC->SetViewTargetWithBlend(NewViewTarget, 0.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+				}
 			}
 		}
 	}
