db406104e956780fe7a0a2eb81e512d6c48ff3cc
Erickson Silva <erickson.lucas@live.com>
2018-01-28 18:50:47 -0200

    56. Line Tracing Part 1

--- CoopGame/Source/CoopGame/SWeapon.h
+++ CoopGame/Source/CoopGame/SWeapon.h
@@ -22,6 +22,9 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
 	USkeletalMeshComponent* MeshComp;
 
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void Fire();
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
